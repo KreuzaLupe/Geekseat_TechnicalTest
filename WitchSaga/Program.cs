ff07df60afb58ae64f61c28ccbc41f147bfb2c0e
kreuza lupe <kreuza.lupe@gmal.com>
2021-09-01 18:43:28 +0200

    Console update

--- WitchSaga/Program.cs
+++ WitchSaga/Program.cs
@@ -7,17 +7,29 @@ namespace WitchSaga
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Press to continue: ");
-            int len = Convert.ToInt32(Console.ReadLine());
             List<Person> lst = new List<Person>();
             Person p1 = new Person();
-            p1.YearOfDeath = 12;
-            p1.Age = 10;
-            lst.Add(p1);
             Person p2 = new Person();
-            p2.Age = 13; p2.YearOfDeath = 17;
+            Console.WriteLine("Enter Person1 age: ");
+            p1.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Person1 Year of Death: ");
+            p1.YearOfDeath = Convert.ToInt32(Console.ReadLine());
+            lst.Add(p1);
+            Console.WriteLine("Enter Person2 age: ");
+            p2.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Person2 Year of Death: ");
+            p2.YearOfDeath = Convert.ToInt32(Console.ReadLine());          
             lst.Add(p2);
-            Console.WriteLine(CoderReturn.averageDeathsperYear(lst));
+            double response = CoderReturn.averageDeathsperYear(lst);
+            if (response!=-1)
+            {
+                Console.WriteLine("The average number of killed peoples is : {0}", CoderReturn.averageDeathsperYear(lst));
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid data");
+            }
             Console.ReadLine();
         }
     
